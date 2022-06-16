f9ef966dcd5527008bb24548cc6dcb333c2f557f
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2022-06-16 19:43:18 +0530

    Added: Java Program

---
+++ Compuation.java
@@ -0,0 +1,17 @@
+class Computation{
+	
+	public static int add(int num1, int num2)
+	{
+		return  num1 + num2;
+	}
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("Welcome to Computation Program");
+		int sum=add(10,20);
+		System.out.println("Sum is: "+sum);
+		
+	}
+	
+}
